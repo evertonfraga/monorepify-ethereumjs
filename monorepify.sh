5a855f0b8b28217c01076721a1e82853e1ee28f2
Everton Fraga <ev@ethereum.org>
2019-12-10 15:42:53 -0500

    Refactor into functions for clarity

--- monorepify.sh
+++ monorepify.sh
@@ -1,41 +1,42 @@
 #!/bin/bash
 
-# exit when any command fails
+# immediately exits when a command fails
 set -e
 
-# Saving current directory
-CWD=`pwd`
-MONOREPO="$CWD/ethereumjs-vm"
+setup_global_vars() {
+  # Saving current directory
+  CWD=`pwd`
+  MONOREPO="$CWD/ethereumjs-vm"
 
-# Log function
-GREEN="\033[0;32m"
-NOCOLOR="\033[0m"
+  # List of repos we want to bring in
+  EXTERNAL_REPOS="account block blockchain tx common"
+  ALL_REPOS="$EXTERNAL_REPOS vm"
+}
+
+# Logging function
 info() {
+  GREEN="\033[0;32m"
+  NOCOLOR="\033[0m"
   echo -e "${GREEN}$1 ${NOCOLOR}"
 }
 
-# List of repos we want to bring in
-EXTERNAL_REPOS="account block blockchain tx common"
-ALL_REPOS="$EXTERNAL_REPOS vm"
+migrate_repos() {
+  # Based on:
+  # https://medium.com/@filipenevola/how-to-migrate-to-mono-repository-without-losing-any-git-history-7a4d80aa7de2
 
-# Migrating repos preserving their git history
-# Based on: https://medium.com/@filipenevola/how-to-migrate-to-mono-repository-without-losing-any-git-history-7a4d80aa7de2
-# 
-# 1. copy repos
-  # 1.1 Clone from original repos
-  info "Cloning all repos... "
+  info "1. Cloning all repos..."
   for REPO in $ALL_REPOS
   do
     git clone git@github.com:ethereumjs/ethereumjs-$REPO.git --single-branch
   done
 
-  # 1.2 Destination repo, monorepo branch
-  info "Creating branch monorepo... "
-  cd $MONOREPO && git checkout -b monorepo-1
-  cd $CWD
 
-  # 1.4 Moving each repo to a subdirectory
-  info "Moving all files 1 directory deeper..."
+  info "2. Creating branch monorepo..."
+  cd $MONOREPO && git checkout -b monorepo
+
+
+  info "3. Moving all files to /package/<name>."
+  cd $CWD
   for REPO in $ALL_REPOS
   do
     mkdir -p $CWD/ethereumjs-$REPO/packages/$REPO
@@ -43,13 +44,13 @@ ALL_REPOS="$EXTERNAL_REPOS vm"
     ls -A1 | grep -Ev "^(packages|\.git)$" | xargs -I{} git mv {} packages/$REPO
     git commit -m "monorepo: moving $REPO"
   done
-  info "OK"
 
-  # 1.5 Renaming Tags
-  info "Renaming tags..."
+
+  info "4. Renaming tags..."
+  COUNTER=1
   for REPO in $ALL_REPOS
   do
-    info "Renaming tags from ethereumjs-$REPO"
+    info "4.$COUNTER Renaming tags from ethereumjs-$REPO"
     cd $CWD/ethereumjs-$REPO
 
     # Remove prefix `v` from tags
@@ -66,20 +67,21 @@ ALL_REPOS="$EXTERNAL_REPOS vm"
     # Remove tags in the old format
     git tag -l | grep -E "^\d+\.\d+\.\d+" | xargs -I{} git tag -d {}
 
+    ((COUNTER+=1))
   done
 
-  # 1.6 Adding other directories as remote
+
+  info "5. Adding other directories as remote..."
   # no need to add VM here, as it is self
-  info "Adding other directories as remote..."
-  cd $MONOREPO
-  
+  cd $MONOREPO  
   for REPO in $EXTERNAL_REPOS
   do
     git remote add $REPO $CWD/ethereumjs-$REPO/
   done
   info "OK"
 
-  # 1.7 Managing remotes
+
+  info "6. Setting up git remotes from local files..."
   # Removing origin 
   git remote remove origin
   # Fetching all new repos from local paths
@@ -88,51 +90,54 @@ ALL_REPOS="$EXTERNAL_REPOS vm"
   git remote add ev git@github.com:evertonfraga/ethereumjs-vm.git
 
 
-  # 1.9 merging "remote" repos in monorepo
-  info "Merging other repos to monorepo..."
-
+  info "7. Merging other repos to monorepo..."
   for REPO in $EXTERNAL_REPOS
   do
     info "Merging ethereumjs-$REPO"
     git merge $REPO/master --no-edit --allow-unrelated-histories
   done
   info "OK"
+}
 
-# 2. Handle files under ./.github
-  info "Handle files under ./.github..."
+migrate_github_files() {
+  info "Migrate Github Files: START"
   cd $MONOREPO
+
+  info "1. Handle files under ./.github..."
   mkdir -p .github/workflows
 
-  # 2.2 Move all .yml files to root
-  info "Move all .github files to root..."
+  info "2. Move all .github files to root..."
   git mv packages/vm/.github/contributing.md .github/
   git mv packages/*/.github/workflows/* .github/workflows
 
-  # 2.3 Remove packages' github dir (with their remaining contributing.md)
-  info "Remove packages' github dir..."
-  git rm -rf packages/*/.github --ignore-unmatch
+  info "3. Remove packages' github dir..."
+  git rm -rf packages/*/.github --ignore-unmatch # also deletes remaining contributing.md files
 
-  info "Committing github changes..."
+  info "4. Committing github changes..."
   git commit -m 'monorepo: Moving .github files to root'
-  info "Handle files under ./.github: DONE"
 
-# 3. Inject paths to ignore for each job, so we don't run out of job runners
-
-  info "Making test cascade changes..."
+  info "5. Making cascade tests changes..."
+  # context: https://github.com/ethereumjs/ethereumjs-vm/issues/561#issuecomment-558943311
   cd $CWD
   node make-test-cascade.js  
   
   cd $MONOREPO
   git commit .github/workflows -m 'monorepo: Adding test cascade directives'
+
+  info "Migrate Github Files: OK"
+}
+
+tear_down_remotes() {
+  info "Tear down remotes..."
+  for REPO in $EXTERNAL_REPOS
+  do
+    git remote remove $REPO
+  done
   info "OK"
+}
 
-
-# X. Rename git tags
-
-
-
-
-# 4. TODO: Changing all link references to the new repo and file structure
+update_link_references() {
+  # Changing all link references to the new repo and file structure
 
   # 4.1 Change repo name 
   # As a commit is pinned, it is OK to keep the file structure
@@ -143,32 +148,15 @@ ALL_REPOS="$EXTERNAL_REPOS vm"
   # Input:    https://github.com/ethereumjs/ethereumjs-block/blob/master/docs/index.md
   # Output-2: https://github.com/ethereumjs/ethereumjs-vm/blob/master/docs/index.md
   # Output-1: https://github.com/ethereumjs/ethereumjs-vm/blob/master/package/block/docs/index.md
+  info "implementation missing."
+}
 
-  # 3.3 Commit changes
+# 
+# Execution starts here
+# 
 
-
-# TODO: 
-# implement lerna
-# merge vscode
-# merge prettier
-# deal with tsconfig
-# deal with tslint
-# make sure CHANGELOG can still be generated
-# Update link references on all .MD files
-# Update link references to the project badges
-# Update link references to package.json
-
-# Tearing down local origins
-  info "Tears down remotes..."
-  for REPO in $EXTERNAL_REPOS
-  do
-    git remote remove $REPO
-  done
-  info "OK"
-
-# Final checks
-for REPO in $ALL_REPOS
-do
-  cd $CWD/ethereumjs-$REPO
-  info "Commits in $REPO: `git rev-list --count HEAD`"
-done
+setup_global_vars
+migrate_repos
+migrate_github_files
+tear_down_remotes
+update_link_references
