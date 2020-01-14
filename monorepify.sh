6aa0337c63c2cb31d7f6b75d900ab284262b2d96
Everton Fraga <ev@ethereum.org>
2020-01-14 00:51:13 -0300

    Change reporting formats with automatic step counter

--- monorepify.sh
+++ monorepify.sh
@@ -20,22 +20,34 @@ info() {
   echo -e "${GREEN}$1 ${NOCOLOR}"
 }
 
-migrate_repos() {
-  # Based on:
-  # https://medium.com/@filipenevola/how-to-migrate-to-mono-repository-without-losing-any-git-history-7a4d80aa7de2
+SECTION=0
+ITEM=0
 
-  info "1. Cloning all repos..."
+section() {
+  ((SECTION+=1))
+  ITEM=0
+  info "${SECTION}. $1"
+}
+item() {
+  ((ITEM+=1))
+  info "${SECTION}.${ITEM}. $1"
+}
+
+migrate_repos() {
+  # Inspired on:
+  # https://medium.com/@filipenevola/how-to-migrate-to-mono-repository-without-losing-any-git-history-7a4d80aa7de2
+  
+  section "Cloning all repos..."
+
   for REPO in $ALL_REPOS
   do
     git clone git@github.com:ethereumjs/ethereumjs-$REPO.git --single-branch
   done
 
-
-  info "2. Creating branch monorepo..."
+  item "Creating branch monorepo..."
   cd $MONOREPO && git checkout -b monorepo
 
-
-  info "3. Moving all files to /package/<name>."
+  item "Moving all files to /package/<name>."
   cd $CWD
   for REPO in $ALL_REPOS
   do
@@ -45,12 +57,10 @@ migrate_repos() {
     git commit -m "monorepo: moving $REPO"
   done
 
-
-  info "4. Renaming tags..."
-  COUNTER=1
+  section "Renaming tags..."
   for REPO in $ALL_REPOS
   do
-    info "4.$COUNTER Renaming tags from ethereumjs-$REPO"
+    item "Renaming tags from ethereumjs-$REPO"
     cd $CWD/ethereumjs-$REPO
 
     # Remove prefix `v` from tags
@@ -67,11 +77,10 @@ migrate_repos() {
     # Remove tags in the old format
     git tag -l | grep -E "^\d+\.\d+\.\d+" | xargs -I{} git tag -d {}
 
-    ((COUNTER+=1))
   done
 
 
-  info "5. Adding other directories as remote..."
+  section "Adding other directories as remote..."
   # no need to add VM here, as it is self
   cd $MONOREPO  
   for REPO in $EXTERNAL_REPOS
@@ -81,54 +90,53 @@ migrate_repos() {
   info "OK"
 
 
-  info "6. Setting up git remotes from local files..."
-  # Removing origin 
-  git remote remove origin
+  section "Setting up git remotes from local files..."
   # Fetching all new repos from local paths
   git fetch --all
   # Adding a destination for the monorepo, ignoring its tags for now
   git remote add ev git@github.com:evertonfraga/ethereumjs-vm.git
 
 
-  info "7. Merging other repos to monorepo..."
+  section "Merging other repos to monorepo..."
   for REPO in $EXTERNAL_REPOS
   do
-    info "Merging ethereumjs-$REPO"
+    item "Merging ethereumjs-$REPO"
     git merge $REPO/master --no-edit --allow-unrelated-histories
   done
   info "OK"
 }
 
-migrate_github_files() {
-  info "Migrate Github Files: START"
+move_github_files() {
+  section "Move Github Files: START"
   cd $MONOREPO
 
-  info "1. Handle files under ./.github..."
+  item "Handle files under ./.github..."
   mkdir -p .github/workflows
 
-  info "2. Move all .github files to root..."
+  item "Move all .github files to root..."
   git mv packages/vm/.github/contributing.md .github/
   git mv packages/*/.github/workflows/* .github/workflows
 
-  info "3. Remove packages' github dir..."
+  item "Remove packages' github dir..."
   git rm -rf packages/*/.github --ignore-unmatch # also deletes remaining contributing.md files
 
-  info "4. Committing github changes..."
+  item "Committing github changes..."
   git commit -m 'monorepo: Moving .github files to root'
+  info "Move Github Files: OK"
+}
 
-  info "5. Making cascade tests changes..."
+make_tests_cascade() {
+  section "Making cascade tests changes..."
   # context: https://github.com/ethereumjs/ethereumjs-vm/issues/561#issuecomment-558943311
   cd $CWD
   node gh-actions-make-cascade-tests.js
   
   cd $MONOREPO
   git commit .github/workflows -m 'monorepo: Adding test cascade directives'
-
-  info "Migrate Github Files: OK"
 }
 
 fix_cwd_github_files() {
-  info "Fix github actions execution paths"
+  section "Fix github actions execution paths"
 
   cd $CWD
   node gh-actions-adjust-paths.js
@@ -139,8 +147,11 @@ fix_cwd_github_files() {
   info "Fix github actions execution paths: OK"
 }
 
-tear_down_remotes() {
-  info "Tear down remotes..."
+tear_down_remotes() { 
+  # That's for additional safety while developing
+  section "Tear down remotes..."
+  cd $MONOREPO
+
   for REPO in $EXTERNAL_REPOS
   do
     git remote remove $REPO
