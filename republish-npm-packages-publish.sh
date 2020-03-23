6c394ade350eca2325eddce89ff0ef00717fb0ad
Everton Fraga <ev@ethereum.org>
2020-03-23 08:59:40 -0400

    Publishing packages

--- republish-npm-packages-publish.sh
+++ republish-npm-packages-publish.sh
@@ -4,13 +4,14 @@ PACKAGE_DIR=$1
 
 cd packages/$PACKAGE_DIR
 
-FILES=find . -iname "*.tgz" | sort
+FILES=`find . -name "*.tgz" | sort`
 
 for FILE in $FILES
 do
     echo -n "PUBLISHING $FILE..." 
-    npm publish --access=public $FILE --dry-run
+    npm publish --access=public $FILE --otp 
     echo "DONE"
+    rm $FILE
 done
 
 echo "DONE"
